168ce43ef5644a53e5cbcd95e8d4456df4b4832f
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-19 12:45:41 -0700

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num >= 0) {
+        let finalStr = "";
+        for (let i = 0; i < num; i++) {
+            finalStr += str;
+        }
+        return finalStr;
+    }
+    else return "ERROR";
 };
-
 // Do not edit below this line
 module.exports = repeatString;
