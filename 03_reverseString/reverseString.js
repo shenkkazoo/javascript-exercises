f8c28cd46416ffb03af57cb1d7de28990aa60702
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-19 12:52:42 -0700

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const splitArray = str.split("");
+    const reverseArray = splitArray.reverse();
+    let newStr = "";
+    for (const chara of reverseArray) {
+        newStr += chara;
+    }    
+    return newStr;
 };
 
 // Do not edit below this line
