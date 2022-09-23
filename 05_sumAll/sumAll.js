bf753fde9a33ce54f44609583fc016a2df39088f
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-23 11:28:45 -0700

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(numOne,numTwo) {
+
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+
+    if (typeof numOne === "string" || typeof numTwo === "string") {
+        return "ERROR";
+    }
+
+    if (isNaN(numOne) || isNaN(numTwo)) {
+        return "ERROR";
+    }
+
+
+    let finalNum = 0;
+
+    const smallNum = (numOne < numTwo) ? numOne : numTwo;
+    const largeNum = (numOne > numTwo) ? numOne : numTwo;
+
+    for (let i = smallNum; i <= largeNum; i++) {
+        finalNum += i;
+    }
+
+    return finalNum;
 
 };
 
