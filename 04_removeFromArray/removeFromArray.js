081f87fff16a65cce0b35c2a9ef422f49e3ead1d
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-23 11:05:26 -0700

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arrayOne = args[0];
 
+    let newArr = [];
+
+    for (const item of arrayOne) {
+            if (!args.includes(item)) {
+                newArr.push(item);
+            }
+        }
+    return newArr;
 };
 
 // Do not edit below this line
