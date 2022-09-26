86b85ee8ca4df62b4105f5ec3933fdc9f08483b9
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-26 13:03:16 -0700

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (temp - 32)/1.8;
+  newTemp = Math.round(newTemp * 10)/10;
+  return newTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = (temp * 1.8)+32;
+  newTemp = Math.round(newTemp * 10)/10;
+  return newTemp;
 };
+
 
 // Do not edit below this line
 module.exports = {
