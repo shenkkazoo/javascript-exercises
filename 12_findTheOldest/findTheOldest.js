8a8629a323cc6775dab9295e34e4affd57a107e3
Cassandra Cawley <cassandrabook09@gmail.com>
2022-10-07 12:11:03 -0700

    Fixed up 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,38 +1,25 @@
 const findTheOldest = function(people) {
     let oldestPerson = people[0];
     oldestPerson = people.reduce((personA, personB) => {
-        let personAAge, personBAge, oldestAge;
-
-        if (personA.yearOfDeath === undefined) {
-            personAAge = 2022 - personA.yearOfBirth;
-        }
-        else {
-            personAAge = personA.yearOfDeath - personA.yearOfBirth;
-        }
-
-        
-        if (personB.yearOfDeath === undefined) {
-            personBAge = 2022 - personB.yearOfBirth;
-        }
-        else {
-            personBAge = personB.yearOfDeath - personB.yearOfBirth;
-        }
-
-
-        if (oldestPerson.yearOfDeath === undefined) {
-            oldestAge = 2022 - oldestPerson.yearOfBirth;
-        }
-        else {
-            oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
-        }
+        const personAAge = getAge(personA);
+        const personBAge = getAge(personB);
+        const oldestAge = getAge(oldestPerson);
 
         if (personAAge > personBAge && oldestAge) { return personA; }
         else if (personBAge > personAAge && oldestAge) { return personB; }
         else { return oldestPerson; }
-        
     });
 
     return oldestPerson;
+};
+
+const getAge = function(person) {
+    if (!person.yearOfDeath) {
+        return 2022 - person.yearOfBirth;
+    }
+    else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
