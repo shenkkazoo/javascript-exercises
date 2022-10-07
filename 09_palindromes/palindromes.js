245d4576c4d2dad98296bcb7726cc6151b998217
Cassandra Cawley <cassandrabook09@gmail.com>
2022-10-07 10:46:06 -0700

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let reverseStr = reverseString(str);
 
+    reverseStr = removeSpecial(reverseStr);
+    str = removeSpecial(str);
+
+    if (str.toLowerCase() === reverseStr.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+function reverseString(str) {
+    const splitArray = str.split("");
+    const reverseArray = splitArray.reverse();
+    let newStr = "";
+    for (const chara of reverseArray) {
+        newStr += chara;
+    }    
+    return newStr;
+}
+
+function removeSpecial(str) {
+    //need to figure out what this regex is actually doing, because it's throwing me for a Loop!
+    const special = /[^\w]/gi;
+    return str.replace(special, '');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
