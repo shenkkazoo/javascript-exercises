ae5dcb00832003eb3897fb1561bac1b9a92fae39
Cassandra Cawley <cassandrabook09@gmail.com>
2022-10-07 11:06:41 -0700

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 0) { return "OOPS"; }
+    let currentNumber = 0;
+    let nextNumber = 1;
+    let sum = 0;
 
+    for (let i = 0; i < member; i++) {
+        sum = currentNumber + nextNumber;
+        currentNumber = nextNumber;
+        nextNumber = sum;
+        console.log(currentNumber);
+    }
+
+    return currentNumber;
 };
 
 // Do not edit below this line
